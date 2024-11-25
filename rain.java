2fdea1e6100e25e9558bd1bba22c99cadff0fadf
RakshnaRavi <Tellrakshna@gmail.com>
2024-11-25 12:24:13 -0500

    Lambda expresssion
    We coded the lambda function to the main method.

--- rain.java
+++ rain.java
@@ -6,3 +6,19 @@ interface NumAdd
 	int numbers(int a, int rVal);
 }
 
+public class rain implements NumAdd
+{
+	public static void main (String [] args)
+	{
+		NumAdd add = (a, rVal) ->
+		{
+			int sum =0;
+			for(int i = a; i < rVal; i++)
+			{
+				sum+=i;
+			}
+			return sum;
+		}
+	}
+}
+
