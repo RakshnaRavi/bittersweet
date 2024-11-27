684de66c718402efb6042b23ede166b82c973f84
RakshnaRavi <Tellrakshna@gmail.com>
2024-11-27 10:47:01 -0500

    changed file name to main

---
+++ Main.java
@@ -0,0 +1,36 @@
+
+@FunctionalInterface
+interface Adder {
+    int addNumber(int start, int end);
+}
+
+public class Main{
+    public static void main(String[] args) {
+		if(args.length < 2)
+		{
+			return;
+		}
+
+		try{
+            int start =  Integer.parseInt(args[0]);;
+            int end =  Integer.parseInt(args[1]);;
+
+            Adder add = (a, b) -> {
+                int sum = 0;
+                for (int i = a; i <= b; i++) {
+                    sum += i;
+                }
+                return sum;
+            };
+
+            int result = add.addNumber(start, end);
+            System.out.println("The sum: " + result);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Error");
+		}
+
+    }
+}
+
