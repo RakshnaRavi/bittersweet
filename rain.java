db752254c5cfe36c83ba0d77ca903e76e254ce21
RakshnaRavi <Tellrakshna@gmail.com>
2024-11-27 10:18:04 -0500

    Code update
    We updated the code so it works as intended and the command line arguments works.

--- rain.java
+++ rain.java
@@ -1,24 +1,37 @@
 
 @FunctionalInterface
-
-interface NumAdd
-{
-	int numbers(int a, int rVal);
+interface Adder {
+    int addNumber(int start, int end);
 }
 
-public class rain implements NumAdd
-{
-	public static void main (String [] args)
-	{
-		NumAdd add = (a, rVal) ->
+public class rain {
+    public static void main(String[] args) {
+		if(args.length < 2)
 		{
-			int sum =0;
-			for(int i = a; i < rVal; i++)
-			{
-				sum+=i;
-			}
-			return sum;
+			return;
 		}
-	}
+
+		try{
+            int start =  Integer.parseInt(args[0]);;
+            int end =  Integer.parseInt(args[1]);;
+
+            Adder add = (a, b) -> {
+                int sum = 0;
+                for (int i = a; i <= b; i++) {
+                    sum += i;
+                }
+                return sum;
+            };
+
+
+            int result = add.addNumber(start, end);
+            System.out.println("The sum: " + result);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Error");
+		}
+
+    }
 }
 
